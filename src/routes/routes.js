cdd770e31c9054d32af18a71875a03ae2aa498fc
epianco <vinicius.dsp19@gmail.com>
2020-09-16 20:57:45 -0300

    Backend Com Update e Delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,5 +8,7 @@ router.post('/usuarios', UsuarioController.Insert);
 router.post('/produtos', ProdutoController.Insert);
 router.get('/usuarios', UsuarioController.SelectAll);
 router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.put('/usuarios/:id', UsuarioController.Update);
+router.delete('/usuarios/:id', UsuarioController.Delete);
 
 module.exports = router;
